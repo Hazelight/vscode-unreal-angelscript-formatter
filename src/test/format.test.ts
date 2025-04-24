dfce192cda8635668bc30e80ecda0a846129f17f
Nils <contact@nilssoderman.com>
2025-04-24 11:26:43 +0200

    Added a first test

---
+++ src/test/format.test.ts
@@ -0,0 +1,50 @@
+import * as assert from 'assert';
+
+import * as vscode from 'vscode';
+
+
+import { AngelscriptClangDocumentFormattingEditProvider } from '../formatter';
+
+suite('Apply Formatting', () => {
+    const currentDir = vscode.Uri.file(__dirname);
+    const formattedFilepath = vscode.Uri.joinPath(currentDir, "..", "..", "test", "fixture", "formatted.as");
+    const unformattedFilepath = vscode.Uri.joinPath(currentDir, "..", "..", "test", "fixture", "unformatted.as");
+
+    let extensionContext: any
+    let document: vscode.TextDocument
+    let expectedFormattedText: string
+
+    setup(async () => {
+        const extension = vscode.extensions.getExtension("Hazelight.unreal-angelscript-clang-format");
+        if (!extension)
+            throw new Error("Extension not found");
+        extensionContext = {
+            extensionUri: extension.extensionUri,
+        }
+
+        document = await vscode.workspace.openTextDocument(unformattedFilepath);
+
+        const formattedDocument = await vscode.workspace.openTextDocument(formattedFilepath);
+        expectedFormattedText = formattedDocument.getText();
+    });
+
+    teardown(() => {
+    });
+
+    test('Format Fixtures', async function () {
+        const formattingProvider = new AngelscriptClangDocumentFormattingEditProvider(extensionContext);
+
+        const edits = await formattingProvider.provideDocumentFormattingEdits(document, {
+            "insertSpaces": true,
+            "tabSize": 4,
+        }, null as any);
+
+        // Apply the edits
+        const workspaceEdit = new vscode.WorkspaceEdit();
+        workspaceEdit.set(document.uri, edits);
+        await vscode.workspace.applyEdit(workspaceEdit);
+
+        const formattedText = document.getText();
+        assert.strictEqual(formattedText, expectedFormattedText, "Formatted text does not match expected text");
+    });
+});
---
+++ test/fixture/unformatted.as
@@ -0,0 +1,44 @@
+int   TestVar =   5  ;
+
+event void  FEventExample ( UObject Object,  float Value);
+delegate  bool FDelegateExample(FVector& OutLocation) ;
+
+void TestFunc() {
+}
+
+  float  TestFunc2   (  int   arg )   {}
+
+ class  Class  :  ParentClass{
+  FName  TestVar1  =  n"Hello  World" ;
+
+  access Internal = private,UObject;
+
+  access:Internal
+	float PrivateFloatValue=0.0;
+	
+
+  access EditAndReadOnly  =  private, * ( editdefaults,readonly);
+
+UPROPERTY (NotEditable,  BlueprintReadOnly,Category="Test", meta=( DisplayName="TestVar2"))
+		protected FVector  TestVar2  = FVector ( 1 , 2,3 )  ;
+
+	UFUNCTION( BlueprintCallable,  Category =  "Test")
+   private void TestFunc3(FVector & TestVar3, const  FHitResult&in   HitResultIn, FHitResult &out HitResultOut) {
+   // Comment
+	if ( TestVar1 == 5 ) {
+		TestVar1 = 10;
+	} else  { Print( f"{TestVar1}" ); }}
+
+
+	private void PrivateFunc()
+	{  }
+
+
+	protected void ProtectedFunc(){
+
+	}
+
+
+FRotator TestRotator=FRotator(0.0,0.0,0.0);
+
+}
